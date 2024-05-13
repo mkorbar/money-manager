dafe4177a11ac65c784391687376b2cba8a1e165
mkorbar <matej.korbar@gmail.com>
2024-05-13 19:25:07 +0200

    Create expense form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css';
-import { React } from 'react';
+import { React, useState } from 'react';
 import List_expences from './components/list_expences/list_expences';
+import AddExpense from './components/add_expence/add_expense';
 function App() {
+
+  let [tab, setTab] = useState('add');
 
   // jsx
   return (
+    <>
+    <ul className="nav nav-tabs m-3">
+      <li className="nav-item">
+        <a className={"nav-link" + (tab == 'list' ? ' active': '')} onClick={() => {setTab('list')}}>List</a>
+      </li>
+      <li className="nav-item">
+        <a className={"nav-link" + (tab == 'add' ? ' active': '')} onClick={() => {setTab('add')}}>Add</a>
+      </li>
+    </ul>
     <div className='container'>
-      
-      <List_expences />
-
+      {
+      tab == 'add' ? <AddExpense /> : <List_expences />
+      }
     </div>
+    </>
   );
 }
 
---
+++ src/components/add_expence/add_expense.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+function AddExpense() {
+
+    let [showSuccessMsg, setShowSuccessMsg] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let expense = {
+            "date": e.target.date.value,
+            "amount": e.target.amount.value,
+            "category": e.target.category.value,
+            "type": e.target.type.value,
+            "title": e.target.title.value,
+            "description": e.target.description.value,
+        }
+        setShowSuccessMsg(true)
+        e.target.reset();
+
+        console.log(expense);
+    }
+
+    return (
+        <div>
+            {showSuccessMsg ? (<>
+                <div className="alert alert-success" role="alert">
+                    Data successfuly saved.
+                </div>
+            </>) : ''}
+
+
+            <form onSubmit={handleSubmit}>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="date">Date</label>
+                    </div>
+                    <div className="col-9">
+                        <input className="form-control" type="date" name="date" id="name" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="category">Category</label>
+                    </div>
+                    <div className="col-9">
+                        <select className="form-select" id="category" name="category">
+                            <option value="groceries">Groceries</option>
+                            <option value="transportation">Transportation</option>
+                            <option value="salary">Salary</option>
+                            <option value="entertainment">Entertainment</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="amount">Amount</label>
+                    </div>
+                    <div className="col-9">
+                        <input className="form-control" type="number" name="amount" id="amount" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="type">Type</label>
+                    </div>
+                    <div className="col-9">
+                        <input type="radio" className="btn-check mx-3" name="type" id="typeExpense" autoComplete="off" />
+                        <label className="btn btn-outline-danger me-1" htmlFor="typeExpense">Expense</label>
+
+                        <input type="radio" className="btn-check mx-3" name="type" id="typeIncome" autoComplete="off" />
+                        <label className="btn btn-outline-primary me-1" htmlFor="typeIncome">Income</label>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="title">Title</label>
+                    </div>
+                    <div className="col-9">
+                        <input className="form-control" type="text" name="title" id="title" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3">
+                        <label htmlFor="description">Description</label>
+                    </div>
+                    <div className="col-9">
+                        <textarea name="description" id='description' className="form-control"></textarea>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button className="btn btn-primary form-control" type="submit">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default AddExpense;
